ace88e5ab058a032d182060245da98f9bba15be4
Abdel-malik <petitsan7@gmail.com>
2017-05-30 15:37:29 +0200

    ajout commentaires - Roue.h

--- Roue.h
+++ Roue.h
@@ -10,32 +10,54 @@
 /************************************/
 /******   definition Classe    ******/
 /************************************/
+
+/** La classe Roue est la classe se raportant à une roue du véhicule simulé,
+cette roue contient les informations utiles pour une manipulation par l'ABS. **/
 class Roue{
-    //attributs
+
+    ///**attributs**///
     double vitesseAngulaire;
     double rayon;
     double glissement;
     BornesDichotomie bornes;
-    //Méthodes
 
+    ///**Méthodes**///
     public:
-    //Constructeurs
+    /*Constructeurs*/
     Roue(){
         bornes = BornesDichotomie();
         vitesseAngulaire = 0;
         rayon = 7;
         glissement = 0;
     };
-    //méthodes
+    /*méthodes*/
 
+    //param :
+    //in : la vitesse du véhicule
+    //but : calculer le glissement de la roue en fonction de la vitesse du véhicule.
     void glissementRoue(double v_auto){
         glissement = (v_auto-(vitesseAngulaire*rayon))/v_auto;
     };
 
+    //param :
+    //in : l'évolution souhaité d'une étape de dichotomie (Serrer / Relacher)
+    //but : modifier la valeur de freinage par dichotomie
     void dichotomie(ChangementPression choix){
         bornes.dichotomie(choix);
     };
-    //getter
+
+    //but : mettre à jour l'intervalle d'exécution de la dichotomie (zone réduisant l'intervalle de pressions applicable)
+    void bornesIntervalleFaible(){
+        bornes.versIntervalleFaible();
+    };
+
+    //but : mettre à jour l'intervalle d'exécution de la dichotomie (zone augmentant l'intervalle de pressions applicable)
+    void bornesIntervalleFort(){
+        bornes.versIntervalleFort();
+    };
+
+    /*getter*/
+
     double getVitesseAngulaire(){
         return vitesseAngulaire;
     };
@@ -45,7 +67,13 @@ class Roue{
     double getGlissement(){
         return glissement;
     };
-    //setter
+
+    double pressionAppliquee(){
+        return bornes.getIndex();
+    };
+
+    /*setter*/
+
     void setVitesseAngulaire(double va){
         vitesseAngulaire = va;
     };
